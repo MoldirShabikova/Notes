a4799283691193fdbb649e73771e8d8cc8c5c7aa
moldir <moldirshb@gmail.com>
2022-06-02 13:10:25 -0400

    added header, search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import NotesList from './components/NotesList';
+import {nanoid} from 'nanoid';
+import Search from './components/Search';
+import Header from './components/Header';
 function App() {
+  const [notes, setNotes ] = useState([{
+    id: nanoid(),
+    text: "This is my first note",
+     date: "15/04/2021"
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+       date: "25/04/2022"
+      },
+      {
+        id: nanoid(),
+        text: "This is my third note",
+         date: "01/08/2020"
+        },
+        {
+          id: nanoid(),
+          text: "This is my fourth note",
+           date: "10/08/2021"
+          }]);
+
+
+            const [searchText, setSearchText] = useState("")
+            const [darkMode, setDarkMode] = useState(false)
+
+          useEffect(()=>{
+            localStorage.setItem('react-notes-app-data', JSON.stringify(notes))
+          },[notes])
+
+          const addNote = (text)=>{
+            const date = new Date();
+            const newNote ={
+              id: nanoid(),
+              text:text,
+              date: date.toLocaleDateString()
+            }
+            const newNotes = [...notes, newNote];
+            setNotes(newNotes)
+          }
+
+
+
+const deleteNote = (id)=>{
+  const newNotes = notes.filter((note)=>note.id !==id)
+setNotes(newNotes)}
+
+
   return (
-    <div className="App">
-  hello perfect!!!
+    <div className={ `${darkMode && 'dark-mode'}`}>
+
+    <div className="container">
+      <Header handleToggle={setDarkMode}/>
+      <Search handleSearchNote={setSearchText} />
+    <NotesList notes={notes.filter((note)=>note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
     </div>
+
+
+    </div>
+
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header({handleToggle}) {
+  return (
+    <div className='header'>
+        
+        <h1 >Notes</h1>
+        <button onClick={()=> handleToggle((previousDarkMode)=> !previousDarkMode)} className='save'> Toggle Mode</button>
+        
+        </div>
+  )
+}
+
+export default Header
